c0b8516554cf01d24b39ec98430f19713c5e0361
subhajit2205 <subhajitdatta05@gmail.com>
2026-03-14 03:57:27 +0000

    Update ParamProxy to include Low::ErrorHandling and proxy metadata

--- lib/proxies/param_proxy.rb
+++ lib/proxies/param_proxy.rb
@@ -9,6 +9,22 @@ module ::Lowkey
   class ParamProxy
     include ::Low::ErrorHandling
 
+    attr_reader :file_path, :start_line, :scope
+     
+    def initialize(file_path: nil, start_line: nil, scope: nil, name:, type:, expression: nil, source: nil, **kwargs)
+      @file_path = file_path
+      @start_line = start_line
+      @scope = scope
+      @name = name
+      @type = type
+      @expression = expression
+      @source = source
+
+      kwargs.each do |key, value|
+       instance_variable_set("@#{key}", value)
+      end
+    end
+
     def error_type
       ::Low::ArgumentTypeError
     end
