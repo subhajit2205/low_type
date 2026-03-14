667042213f6bdecc25944494f0d60b2b2e2cdd45
subhajit2205 <subhajitdatta05@gmail.com>
2026-03-14 03:58:02 +0000

    Update ReturnProxy to support Lowkey v0.4.0 proxy structure

--- lib/proxies/return_proxy.rb
+++ lib/proxies/return_proxy.rb
@@ -9,6 +9,19 @@ module ::Lowkey
   class ReturnProxy
     include ::Low::ErrorHandling
 
+    attr_accessor :expression, :value
+    attr_reader :file_path, :start_line, :scope, :name
+    
+    def initialize(file_path: nil, start_line: nil, scope: nil, name:, expression: nil, value: nil, source: nil)
+     @file_path = file_path
+     @start_line = start_line
+     @scope = scope
+     @name = name
+     @expression = expression
+     @value = value
+     @source = source
+    end
+
     def error_type
       ::Low::ReturnTypeError
     end
